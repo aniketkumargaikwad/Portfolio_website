7f5e99aab64719536fe3f949396caa466b70c49d
Aniket Gaikwad <aniketg721@gmail.com>
2024-10-28 23:45:59 +0530

    routing # strategt

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,4 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-import { HashLocationStrategy, LocationStrategy } from '@angular/common';
-
+import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { SkillsComponent } from './skills/skills.component';
@@ -9,19 +6,12 @@ import { PortfolioComponent } from './portfolio/portfolio.component';
 import { ContactComponent } from './contact/contact.component';
 import { NotfoundComponent } from './notfound/notfound.component';
 
-const routes: Routes = [
+export const routes: Routes = [
     { path: 'home', component: HomeComponent },
     { path: 'about', component: AboutComponent },
     { path: 'skills', component: SkillsComponent },
     { path: 'portfolio', component: PortfolioComponent },
     { path: 'contact', component: ContactComponent },
-    { path: '', redirectTo: '/home', pathMatch: 'full' },
+    { path: '', component: HomeComponent},
     { path: '**', component: NotfoundComponent }
 ];
-
-@NgModule({
-    imports: [RouterModule.forRoot(routes)],
-    exports: [RouterModule],
-    providers: [{ provide: LocationStrategy, useClass: HashLocationStrategy }]
-})
-export class AppRoutingModule { }
